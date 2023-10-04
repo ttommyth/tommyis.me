a703aff20a191d4f40aa54e44ce00d0d2bbb21c2
ttommyth <ttommyth@gmail.com>
2023-10-04 10:54:28 +0800

    feat: updated vertical roll

--- src/components/client/VerticalRoll.tsx
+++ src/components/client/VerticalRoll.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { easeInOut, motion, useTime, useTransform } from "framer-motion";
+import { easeInOut, motion, useAnimate, useMotionValue, useTime, useTransform } from "framer-motion";
 import { FC, useState } from "react"
 import {maxBy} from "lodash";
 
@@ -7,19 +7,17 @@ export const VerticalRoll:FC<{
   messages:string[]
 }> = (props)=>{
   const {messages} = props;
-  const [currentMsgIndex, setCurrentMsgIndex] = useState(0);
-  const time = useTime();
-  const translateY = useTransform(time, [0, 4000], ['0%', '-100%'], {ease: easeInOut});
-
+  
   return <span className="relative inline-block overflow-hidden">
-    <span className="invisible">{maxBy(messages, it=>it.length)}</span>
-    <motion.span animate={{ translateY: '-100%' }} transition={{repeat: Infinity, duration: 4, ease: easeInOut }} 
-      className="absolute w-full left-0" onAnimationIterationCapture={()=>console.log("what")} >
-      {messages[currentMsgIndex]}
-    </motion.span>
-    <motion.span animate={{ translateY: '-100%' }} transition={{repeat: Infinity, duration: 4, ease: easeInOut }} 
-      className="absolute w-full left-0 top-[100%]" >
-      {messages[currentMsgIndex+1 >= messages.length? 0: currentMsgIndex+1]}
-    </motion.span>
+    <motion.div  animate={{ translateY: [...messages.map((_,idx)=>`-${100*idx}%`),  `-${100 * messages.length}%`]}} transition={{repeat: Infinity, duration: 4* messages.length, ease: easeInOut }}  className="h-auto">
+      <span className="invisible">{maxBy(messages, it=>it.length)}</span>
+      {messages.map((it, idx)=><span className="absolute block" style={{top: `${100 * idx}%`}}
+        key={idx}>
+        {it}
+      </span>)}
+      <span className="absolute block" style={{top: `${100 * messages.length}%`}}>
+        {messages[0]}
+      </span>
+    </motion.div>
   </span>
 }
