9ce0b2c86fbb304a67bdc1852e5ff71f3674d4d5
ttommyth <ttommyth@gmail.com>
2023-10-05 10:43:40 +0800

    fix: seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
 import {redirect} from 'next/navigation';
+
+export const metadata: Metadata = {
+  title: 'Tommy is me',
+  description: 'The portfolio page of Tommy the developer',
+}
 
 // This page only renders when the app is built statically (output: 'export')
 export default function RootPage() {
