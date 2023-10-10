7cdc9a70ecced863cdbbc6e9708778b350805944
ttommyth <ttommyth@gmail.com>
2023-10-10 17:05:46 +0800

    feat: added kofi

--- src/components/client/sections/Hero.tsx
+++ src/components/client/sections/Hero.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import job from "public/image/job_developer.png";
 import { ReactComponent as  GithubLogo } from "public/icon/github.svg";
 import { ReactComponent as  LinkedinLogo } from "public/icon/linkedin.svg";
+import { ReactComponent as  KofiLogo } from "public/icon/kofi.svg";
 import { ArrowSmallRightIcon } from '@heroicons/react/24/solid'
 import { DarkModeSwitch } from "@/hooks/DarkModeHook";
 import { FC } from "react";
@@ -25,8 +26,9 @@ export const Hero:FC<{
           <h1 className="text-6xl font-black">{locale.title2}</h1>
           <h2 className="flex items-bottom gap-2">{locale.iam} <VerticalRoll messages={locale.iamArray}/></h2>
           <ul className="flex gap-2 mt-4">
-            <li><Link href="https://github.com/ttommyth"><GithubLogo className="w-8 fill-current" /> </Link></li>
-            <li><Link href="https://www.linkedin.com/in/ttommyth/"><LinkedinLogo className="w-8 fill-current" /> </Link></li>
+            <li><Link href="https://github.com/ttommyth"><GithubLogo className="w-8 h-8 fill-current" /> </Link></li>
+            <li><Link href="https://www.linkedin.com/in/ttommyth/"><LinkedinLogo className="w-8 h-8 fill-current" /> </Link></li>
+            <li><Link href="https://ko-fi.com/ttommyth"><KofiLogo className="w-8 h-8 fill-current" /> </Link></li>
           </ul>
         </div>
         <div className="w-72 relative">
