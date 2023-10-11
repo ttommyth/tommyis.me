4a1d922f9bb659e600e2c4154af45b84030ed9ef
ttommyth <ttommyth@gmail.com>
2023-10-11 17:46:27 +0800

    fix: recaptcha error

--- src/app/api/contact/route.tsx
+++ src/app/api/contact/route.tsx
@@ -4,9 +4,11 @@ import { NextResponse } from "next/server";
 // To handle a POST request to /api
 export async function POST(request: Request) {
   const body = await request.json();
-  const verifyToken = await axios.post("https://www.google.com/recaptcha/api/siteverify", {
-    secret: process.env.RECAPTCHA_SERVER_KEY,
-    response: body.recaptcha
+  const verifyToken = await axios.post("https://www.google.com/recaptcha/api/siteverify",{},{
+    params: {
+      secret: process.env.RECAPTCHA_SERVER_KEY,
+      response: body.recaptcha
+    }
   });
   if(verifyToken.data.success){
     await axios.post(process.env.DISCORD_WEBHOOK_FEEDBACK_URL!, {
