105ead5a79178f5bc50313f472c84cdfe3807e07
ttommyth <ttommyth@gmail.com>
2023-10-17 16:53:45 +0800

    fix: not found point location

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ import {PiArrowBendLeftUpBold} from "react-icons/pi";
 
 export default function NotFound() {
   return <div className='h-[100dvh] w-full flex  flex-col items-center justify-center gap-8 relative'>
-    <span className="absolute top-appbar left-0 text-xs flex items-end pl-8"><PiArrowBendLeftUpBold className="w-icon h-icon inline-block mb-1"/>Click here to back to homepage</span>
+    <span className="absolute top-appbar left-0 text-xs flex items-end ml-8 lg:-ml-16"><PiArrowBendLeftUpBold className="w-icon h-icon inline-block mb-1"/>Click here to back to homepage</span>
     <Image src="/image/404_not_found.png" alt={"not found"}
       width={0}
       height={0}
